14bec3935feaa76fb2061d23fc76157531856a30
Alexander Todorov <atodorov@redhat.com>
2016-07-13 11:22:52 +0300

    Add test_default_burger_creation_and_check_default_values()
    This test makes sure we actually verify that by default a burger
    includes only 1 pieces of ham.

--- tests/test_burger.py
+++ tests/test_burger.py
@@ -6,5 +6,10 @@ class TestBurger(unittest.TestCase):
         burger = sandwich.Burger().make_sandwich()
         self.assertTrue(burger['type'] == 'burger')
 
+    def test_default_burger_creation_and_check_default_values(self):
+        burger = sandwich.Burger().make_sandwich()
+        self.assertEqual(burger['type'], 'burger')
+        self.assertEqual(burger['ham'], 1)
+
 if __name__ == "__main__":
     unittest.main()
