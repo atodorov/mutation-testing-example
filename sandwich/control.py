8417663019270744dff6e84eedd981a1e0a12730
Alexander Todorov <atodorov@redhat.com>
2016-07-19 16:13:39 +0300

    Another mutant survivor reproducer
    Not using importlib.import_module and not modifying sys.path but
    importing the ham module from control.py like so
    
    	from sandwich.ham import ham
    
    Then execute Cosmic Ray like this
    
    $ cosmic-ray run --baseline=10 ham.json sandwich/ham/ham.py -- tests
    $ cosmic-ray report --full-report ham.json
    job ID 1:Outcome.SURVIVED:sandwich.ham.ham
    command: cosmic-ray worker sandwich.ham.ham number_replacer 0 unittest -- tests
    --- mutation diff ---
    --- a/home/atodorov/mutation-testing-example/sandwich/ham/ham.py
    +++ b/home/atodorov/mutation-testing-example/sandwich/ham/ham.py
    @@ -3,6 +3,6 @@
    
     class Ham(object):
    
    -    def __init__(self, pieces=10):
    +    def __init__(self, pieces=11):
             self.pieces = pieces
    
    total jobs: 1
    complete: 1 (100.00%)
    survival rate: 100.00%

--- sandwich/control.py
+++ sandwich/control.py
@@ -1,11 +1,16 @@
-import os
-import sys
-import importlib
+#import os
+#import sys
+#import importlib
 
-path = os.path.dirname(__file__)
-path = os.path.join(path, "ham")
-if not path in sys.path:
-    sys.path.append(path)
+#path = os.path.dirname(__file__)
+#path = os.path.join(path, "ham")
+#if not path in sys.path:
+#    sys.path.append(path)
 
-module = importlib.import_module('ham')
-ham_class = module.__dict__[module.__all__[0]]
+#module = importlib.import_module('sandwich.ham.ham')
+#ham_class = module.__dict__[module.__all__[0]]
+
+from sandwich.ham import ham
+ham_class = ham.Ham
+
+
